4bc74cfe3c0842dc8682750320f51dd6555adc33
dkaraush <dkaraush@gmail.com>
2019-08-21 01:23:38 +0300

    Updates

---
+++ precache-manifest.d00ee665172118c5f0828f44ba7f65a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d6afcd6b66d3c46eaff",
+    "url": "/static/js/main.1650998d.chunk.js"
+  },
+  {
+    "revision": "fb12f5bf1b1cd7826771",
+    "url": "/static/js/2.7d548028.chunk.js"
+  },
+  {
+    "revision": "1d6afcd6b66d3c46eaff",
+    "url": "/static/css/main.8d3c50d7.chunk.css"
+  },
+  {
+    "revision": "351aeb823b7838f0511f78b82a4d4605",
+    "url": "/index.html"
+  }
+];
